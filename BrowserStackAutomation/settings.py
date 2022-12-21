4a4cc7075d87e96b6673108219eea9253c8fe549
kartikkumar-shetty <kartikkumar.shetty@gmail.com>
2022-12-21 15:02:23 +0530

    removing the secret key

--- BrowserStackAutomation/settings.py
+++ BrowserStackAutomation/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-89)%8-e6cs)dzmsu$@212nrwi0g_t)fb(ka=xch!shw_6#w--6"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
