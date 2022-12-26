a25bcf796d8fb63bfd8e548b0fec801452b9191b
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2022-12-26 14:25:08 +0530

    Add schema validator.py

---
+++ validator.py
@@ -0,0 +1,22 @@
+import json
+import os
+import sys
+from jsonschema import validate
+
+try:
+  f = open("schema.json","r")
+  schema = json.load(f)
+  f = open("config.json", "r")
+  config = json.load(f)
+  modules = [ f.path for f in os.scandir("../enigma-public-access-modules") if f.is_dir() ]
+  for module in modules:
+    f = open(module+"/schema.json")
+    module_schema = json.load(f)
+    schema["properties"].update(module_schema["properties"])
+    schema["required"] += module_schema["required"]
+  validate(instance=config, schema=schema)  
+  print("Schema validation passed!")
+except Exception as e:
+  print("Schema validation failed!")
+  print(str(e))
+  sys.exit(1)
