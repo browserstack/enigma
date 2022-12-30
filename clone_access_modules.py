13e4bd393efa3082f246e799427eb333e7a182b2
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2022-12-30 13:06:27 +0530

    updating config validation logic

--- clone_access_modules.py
+++ clone_access_modules.py
@@ -8,7 +8,10 @@ try:
   urls = config["access_modules"]["git_urls"]
   for url in urls:
     folder_name = url.split("/").pop()[:-4]
-    Repo.clone_from(url, "./Access/access_modules/"+folder_name)
+    try:
+      Repo.clone_from(url, "./Access/access_modules/"+folder_name)
+    except Exception as e:
+      print("failed cloning "+folder_name+".")
 except Exception as e:
   print("Access module cloning failed!")
   print(str(e))
