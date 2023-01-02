6d2961318e8ad9fc37f7d62342e9fa08a771959d
kartikkumar shetty <Kartikkumar-Shetty@users.noreply.github.com>
2023-01-02 13:28:18 +0530

    feat: Adding unit tests, views and templates for access status, user list

---
+++ Access/accessrequest_helper.py
@@ -0,0 +1,61 @@
+from Access import helpers
+import logging
+from Access.models import UserAccessMapping, User
+
+logger = logging.getLogger(__name__)
+
+def requestAccessGet(request):
+    context = {}
+    try:
+        for each_access in helpers.getAvailableAccessModules():
+            if "access_" + each_access.tag() in request.GET.getlist('accesses'):
+                if 'accesses' not in context:
+                    context['accesses'] = []
+                    context['genericForm'] = True
+                    try:
+                        extra_fields = each_access.get_extra_fields()
+                    except:
+                        extra_fields = []
+                    try:
+                        notice = each_access.get_notice()
+                            
+                    except Exception:
+                        notice = ""
+                    context['accesses'].append({
+                            'formDesc': each_access.access_desc(),
+                            'accessTag': each_access.tag(),
+                            'accessTypes': each_access.access_types(),
+                            'accessRequestData': each_access.access_request_data(request, is_group=False),
+                            'extraFields': extra_fields,
+                            'notice': notice,
+                            'accessRequestPath': each_access.fetch_access_request_form_path()
+                        })    
+    except Exception as e:
+        logger.exception(e)
+        context = {}
+        context['status'] = {'title':'Error Occured', 'msg': 'There was an error in getting the requested access resources. Please contact Admin'}
+    return context
+
+
+def getGrantFailedRequests(request):
+    try:
+        failures = UserAccessMapping.objects.filter(status__in=['grantfailed']).order_by('-requested_on')
+        if request.GET.get('username'):
+            user = User.objects.get(user__username=request.GET.get('username'))
+            failures = failures.filter(user=user).order_by('-requested_on')
+        if request.GET.get('access_type'):
+            access_tag = request.GET.get('access_type')
+            failures = failures.filter(access__access_tag=access_tag).order_by('-requested_on')
+
+        context = {
+            'failures': failures,
+            'heading': "Grant Failures"
+        }
+        return context
+    except Exception as e:
+        logger.debug("Error in request not found OR Invalid request type")
+        logger.exception(e)
+        json_response = {}
+        json_response['error'] = {'error_msg': str(e), 'msg': "Error in request not found OR Invalid request type"}
+        return json_response
+    
---
+++ Access/tests/test_userlist_helper.py
@@ -0,0 +1,65 @@
+import pytest
+from Access.userlist_helper import getAllUserList, ERROR_MESSAGE, EXCEPTION_USER_UNAUTHORIZED
+
+#TESTCASE NAMES
+UserIsNotAuthorized = "UserIsNotAuthorized"
+UserIsAuthorized = "UserIsAuthorized"
+
+@pytest.mark.parametrize("testname, UserHasPermission, UserHasOffboardPermission,expectedError",
+                        
+    [
+        # user does not have permission to view the page
+        (UserIsNotAuthorized, False, False,True),
+        # user is authorized, and has offboard access
+        (UserIsAuthorized, True, True, False),
+        # user is authorized, but does not have offboard access
+        (UserIsAuthorized, True, False, False)
+    ])
+def test_getAllUserList(mocker, testname ,UserHasPermission, UserHasOffboardPermission, expectedError):
+    request = mocker.MagicMock()
+    userMock = mocker.MagicMock()
+    if testname == UserIsNotAuthorized:
+        mocker.patch("Access.helpers.check_user_permissions", return_value=UserHasPermission)
+        request.user = userMock
+
+    elif testname == UserIsAuthorized:
+        
+        userMock.name = "UserNickName"
+        userMock.user.name = "UserNickName"
+        userMock.user.first_name = "UserFirstName"
+        userMock.user.last_name = "UserLastName"
+        userMock.email = "UserEmail"
+        userMock.user.username = "UserName1"
+        userMock.gitusername = "GitUserName"
+        userMock.offbaord_date = "TodaysDate"
+        userMock.current_state.return_value = "Active"
+        userMock.user.is_active = True
+        userMock.user.has_permission.return_value = UserHasOffboardPermission
+        
+        request.user = userMock
+
+        
+        mocker.patch("Access.helpers.check_user_permissions", return_value=UserHasPermission)
+        mocker.patch("Access.models.User.objects.all", return_value=[userMock])
+
+    context = getAllUserList(request)
+    if expectedError:
+        assert context["error"]["msg"]  == ERROR_MESSAGE
+        assert context["error"]["error_msg"]  == EXCEPTION_USER_UNAUTHORIZED
+    else:
+        assert context["dataList"][0]["name"] == userMock.name
+        assert context["dataList"][0]["first_name"] == userMock.user.first_name
+        assert context["dataList"][0]["last_name"] == userMock.user.last_name
+        assert context["dataList"][0]["email"] == userMock.email
+        assert context["dataList"][0]["username"] == userMock.user.username
+        assert context["dataList"][0]["git_username"] == userMock.gitusername
+        assert context["dataList"][0]["offbaord_date"] == userMock.offbaord_date
+        assert context["dataList"][0]["state"] == userMock.current_state()
+        assert context["dataList"][0]["is_active"] == userMock.user.is_active
+        if UserHasOffboardPermission:
+            assert context["viewDetails"]["numColumns"] == 8
+        else:
+            assert context["viewDetails"]["numColumns"] == 7
+
+
+
