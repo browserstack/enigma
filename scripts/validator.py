95a6868a79db6b846e915ed3c273d33a9c26203f
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2023-01-04 11:58:16 +0530

    Updating config.json logic

--- scripts/validator.py
+++ scripts/validator.py
@@ -4,9 +4,10 @@ import sys
 from jsonschema import validate
 
 try:
+  config_file = "config.json" if len(sys.argv) <=1 else sys.argv[1]
   f = open("./schema.json","r")
   schema = json.load(f)
-  f = open("./config.json", "r")
+  f = open("./"+config_file, "r")
   config = json.load(f)
   root_folders = [ f.path for f in os.scandir("./Access/access_modules") if f.is_dir() ]
   for folder in root_folders:
