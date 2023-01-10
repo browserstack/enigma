ccdd7950d71612bc2502d09ac45138146d6c3f3f
Khushhal Maheshwari <khushhal000maheshwari@gmail.com>
2023-01-10 16:00:09 +0530

    feat: clone access modules in docker-entrypoint

--- scripts/clone_access_modules.py
+++ scripts/clone_access_modules.py
@@ -9,12 +9,29 @@ try:
   urls = config["access_modules"]["git_urls"]
   for url in urls:
     folder_name = url.split("/").pop()[:-4]
+    folder_path = "./Access/access_modules/"+folder_name
     try:
-      if os.path.exists("./Access/access_modules/"+folder_name):
-        Repo("./Access/access_modules/"+folder_name).remotes.origin.pull()
+      if os.path.exists(folder_path):
+        Repo(folder_path).remotes.origin.pull()
       else:
-        Repo.clone_from(url, "./Access/access_modules/"+folder_name)
+        Repo.clone_from(url, folder_path)
+        # move all folders, not files in the cloned repo to the access_modules folder except the .git, .github and secrets folder
+        for file in os.listdir(folder_path):
+          if os.path.isdir(folder_path+"/"+file) and file != ".git" and file != ".github" and file != "secrets":
+            os.rename(folder_path+"/"+file, "./Access/access_modules/"+file)
+
+        # remove the cloned repo folder entirely with all its contents which includes folders and files
+        for root, dirs, files in os.walk(folder_path, topdown=False):
+          for file in files:
+            os.remove(os.path.join(root, file))
+          for dir in dirs:
+            os.rmdir(os.path.join(root, dir))
+        os.rmdir(folder_path)
+
+      print("Clonning successful!")
+
     except Exception as e:
+      print(e)
       print("failed cloning "+folder_name+".")
 except Exception as e:
   print("Access module cloning failed!")
