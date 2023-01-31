effd4839c4211d1d55fbcd9f5cc1527ec3cda819
Khushhal Maheshwari <khushhal000maheshwari@gmail.com>
2023-01-31 13:44:49 +0530

    fix: moved site registration to Access.admin

--- Access/admin.py
+++ Access/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from Access.models import User
+from Access.models import (
+    User,
+    Permission,
+    UserAccessMapping,
+    Role,
+    AccessV2,
+    GroupV2,
+    MembershipV2,
+)
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -9,3 +17,9 @@ class UserAdmin(admin.ModelAdmin):
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Permission)
+admin.site.register(UserAccessMapping)
+admin.site.register(Role)
+admin.site.register(AccessV2)
+admin.site.register(GroupV2)
+admin.site.register(MembershipV2)
