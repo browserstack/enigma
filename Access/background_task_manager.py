061f644e3253e0005b29d9d2dab66e85dc707c70
Khushhal Maheshwari <khushhal000maheshwari@gmail.com>
2023-02-01 15:08:26 +0530

    refactor: moved run_access_grant to background_task_manager

--- Access/background_task_manager.py
+++ Access/background_task_manager.py
@@ -1,21 +1,27 @@
 import json
 import threading
+import traceback
+import logging
 
 from celery import shared_task
 from celery.signals import task_success, task_failure
 
-from Access.views_helper import run_access_grant
+from Access import helpers
+from bootprocess import general
+
+logger = logging.getLogger(__name__)
 
 with open("config.json") as data_file:
-    data = json.load(data_file)
-
-background_task_manager = data["background_task_manager"]["type"]
+    background_task_manager_type = json.load(data_file)["background_task_manager"]["type"]
 
 
-def background_task(func, args):
-    if background_task_manager == "celery":
+def background_task(func, *args):
+    if background_task_manager_type == "celery":
         if func == "run_access_grant":
-            run_access_grant_celery_task.delay(args)
+            run_access_grant.delay(*args)
+
+        if func == "test_grant":
+            test_grant.delay(*args)
 
     else:
         if func == "run_access_grant":
@@ -28,26 +34,132 @@ def background_task(func, args):
 @shared_task(
     autoretry_for=(Exception,), retry_kwargs={"max_retries": 3, "countdown": 5}
 )
-def run_access_grant_celery_task(args):
-    print("run_access_grant running")
-    run_access_grant(args)
-    return
+def run_access_grant(requestId, requestObject, accessType, user, approver):
+    message = ""
+    if not requestObject.user.state == "1":
+        requestObject.status = "Declined"
+        requestObject.save()
+        logger.debug(
+            {
+                "requestId": requestId,
+                "status": "Declined",
+                "by": approver,
+                "response": message,
+            }
+        )
+        return False
+    for each_access_module in helpers.getAvailableAccessModules():
+        if accessType == each_access_module.tag():
+            try:
+                response = each_access_module.approve(
+                    user,
+                    [requestObject.access.access_label],
+                    approver,
+                    requestId,
+                    is_group=False,
+                )
+                if type(response) is bool:
+                    approve_success = response
+                else:
+                    approve_success = response[0]
+                    message = str(response[1])
+            except Exception:
+                logger.exception(
+                    "Error while running approval module: "
+                    + str(traceback.format_exc())
+                )
+                approve_success = False
+                message = str(traceback.format_exc())
+            if approve_success:
+                requestObject.status = "Approved"
+                requestObject.save()
+                logger.debug(
+                    {
+                        "requestId": requestId,
+                        "status": "Approved",
+                        "by": approver,
+                        "response": message,
+                    }
+                )
+            else:
+                requestObject.status = "GrantFailed"
+                requestObject.save()
+                logger.debug(
+                    {
+                        "requestId": requestId,
+                        "status": "GrantFailed",
+                        "by": approver,
+                        "response": message,
+                    }
+                )
+                try:
+                    destination = [
+                        each_access_module.access_mark_revoke_permission(accessType)
+                    ]
+                    subject = str("Access Grant Failed - ") + accessType.upper()
+                    body = (
+                        "Request by "
+                        + user.email
+                        + " having Request ID = "
+                        + requestId
+                        + " is GrantFailed. Please debug and rerun the grant.<BR/>"
+                    )
+                    body = body + "Failure Reason - " + message
+                    body = (
+                        body
+                        + "<BR/><BR/> <a target='_blank'"
+                        + "href "
+                        + "='https://enigma.browserstack.com/resolve/pendingFailure?access_type="
+                        + accessType
+                        + "'>View all failed grants</a>"
+                    )
+                    logger.debug(
+                        "Sending Grant Failed email - "
+                        + str(destination)
+                        + " - "
+                        + subject
+                        + " - "
+                        + body
+                    )
+                    general.emailSES(destination, subject, body)
+                except Exception:
+                    logger.debug(
+                        "Grant Failed - Error while sending email - "
+                        + requestId
+                        + "-"
+                        + str(str(traceback.format_exc()))
+                    )
 
-@task_success.connect(sender=run_access_grant_celery_task)
+            # For generic modules, approve method will send an email on "Access granted",
+            # additional email of "Access approved" is not needed
+            return True
+    return False
+
+@task_success.connect(sender=run_access_grant)
 def task_success(sender=None, **kwargs):
     success_func()
-    print("you are in task success middleman")
+    logger.info("you are in task success middleman")
     return
 
-
-@task_failure.connect(sender=run_access_grant_celery_task)
+@task_failure.connect(sender=run_access_grant)
 def task_failure(sender=None, **kwargs):
     fail_func()
-    print("you are in task fail middleman")
+    logger.info("you are in task fail middleman")
     return
 
 def success_func():
-    print("task successful")
+    logger.info("task successful")
 
 def fail_func():
-    print("task failed")
+    logger.info("task failed")
+
+@shared_task(
+    autoretry_for=(Exception,), retry_kwargs={"max_retries": 3, "countdown": 5}
+)
+def test_grant():
+    for each_access_module in helpers.getAvailableAccessModules():
+        logger.info(each_access_module.tag())
+        if each_access_module.tag() == 'confluence_module':
+            # call access_desc method of confluence module here
+            # and return the result to the caller of this function
+            return each_access_module.access_desc()
