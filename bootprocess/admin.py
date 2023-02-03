716cbc95361c94b29da8dc9e3f8d5c8738fdac42
kartikkumar-shetty <kartikkumar.shetty@gmail.com>
2023-02-03 14:46:44 +0530

    adding user_identity to django dashboard

--- bootprocess/admin.py
+++ bootprocess/admin.py
@@ -8,7 +8,8 @@ from Access.models import (
     AccessV2,
     GroupV2,
     MembershipV2,
-    GroupAccessMapping
+    GroupAccessMapping,
+    UserIdentity
 )
 
 admin.site.register(Permission)
@@ -18,3 +19,4 @@ admin.site.register(AccessV2)
 admin.site.register(GroupV2)
 admin.site.register(MembershipV2)
 admin.site.register(GroupAccessMapping)
+admin.site.register(UserIdentity)
