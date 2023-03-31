48d0a41447fe790c467ddff41fe23db162d8614b
Harish Ved <ved.harish3@gmail.com>
2023-03-31 18:06:30 +0530

    fix: Updated search breaking for json fields

--- Access/admin.py
+++ Access/admin.py
@@ -26,8 +26,8 @@ class UserAdmin(admin.ModelAdmin):
 
 class MembershipV2AdminPanel(admin.ModelAdmin):
     '''Class to describe how to display MembershipV2 on admin panel'''
-    ordering = ("membership_id", "user", "group")
-    search_fields = ("membership_id", "user", "group")
+    ordering = ("membership_id", "user__name", "group__name")
+    search_fields = ("membership_id", "user__name", "group__name")
     list_display = ("membership_id", "user", "group")
 
 
@@ -35,17 +35,41 @@ class AccessV2AdminPanel(admin.ModelAdmin):
     '''Class to describe how to display AccessV2 model on admin panel'''
     search_fields = ("access_tag", "access_label")
     list_display = ("access_tag", "access_label")
+    sortable_by = ("access_tag",)
 
 
 class UserIdentityAdminPanel(admin.ModelAdmin):
     '''Class to describe how to display User Identity on admin panel'''
-    search_fields = ("access_tag", "user", "identity", "status")
+    search_fields = ("access_tag", "user__name", "status")
     list_display = ("access_tag", "user", "identity", "status")
+
+
+class UserAccessMappingAdminPanel(admin.ModelAdmin):
+    '''Class to describe how to display UserAccessMapping on admin panel'''
+    search_fields = (
+        "request_id",
+        "user_identity__user__name",
+        "access__access_tag",
+        "access__access_label",
+        "status"
+    )
+    list_display = (
+        "request_id",
+        "get_user_name",
+        "access",
+        "status",
+    )
+    ordering = (
+        "request_id",
+        "user_identity__user__name",
+        "access",
+        "status"
+    )
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Permission)
-admin.site.register(UserAccessMapping)
+admin.site.register(UserAccessMapping, UserAccessMappingAdminPanel)
 admin.site.register(Role)
 admin.site.register(AccessV2, AccessV2AdminPanel)
 admin.site.register(GroupV2)
