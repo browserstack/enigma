3cf0ec823a8c88318c894a3cdba082f0ce777bc1
Harish Ved <ved.harish3@gmail.com>
2023-03-31 11:55:17 +0530

    feat: Updating admin panel to show model details

--- Access/admin.py
+++ Access/admin.py
@@ -1,3 +1,7 @@
+'''
+Models to configure diplay, search and filtering for models on admin panel
+'''
+
 from django.contrib import admin
 
 from Access.models import (
@@ -14,17 +18,37 @@ from Access.models import (
 
 
 class UserAdmin(admin.ModelAdmin):
+    '''Class to describe how to display User model on admin panel'''
     ordering = ("name", "email")
     search_fields = ("name", "email")
     list_display = ("name", "email")
+
+
+class MembershipV2AdminPanel(admin.ModelAdmin):
+    '''Class to describe how to display MembershipV2 on admin panel'''
+    ordering = ("membership_id", "user", "group")
+    search_fields = ("membership_id", "user", "group")
+    list_display = ("membership_id", "user", "group")
+
+
+class AccessV2AdminPanel(admin.ModelAdmin):
+    '''Class to describe how to display AccessV2 model on admin panel'''
+    search_fields = ("access_tag", "access_label")
+    list_display = ("access_tag", "access_label")
+
+
+class UserIdentityAdminPanel(admin.ModelAdmin):
+    '''Class to describe how to display User Identity on admin panel'''
+    search_fields = ("access_tag", "user", "identity", "status")
+    list_display = ("access_tag", "user", "identity", "status")
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Permission)
 admin.site.register(UserAccessMapping)
 admin.site.register(Role)
-admin.site.register(AccessV2)
+admin.site.register(AccessV2, AccessV2AdminPanel)
 admin.site.register(GroupV2)
-admin.site.register(MembershipV2)
+admin.site.register(MembershipV2, MembershipV2AdminPanel)
 admin.site.register(GroupAccessMapping)
-admin.site.register(UserIdentity)
+admin.site.register(UserIdentity, UserIdentityAdminPanel)
