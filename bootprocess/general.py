42459dec62f8d76f332ac60d6b11c0d907bbcd96
sumedha-bhatt <sumedha@browserstack.com>
2023-03-31 10:37:09 +0530

    refactor: add content type as html

--- bootprocess/general.py
+++ bootprocess/general.py
@@ -8,9 +8,11 @@ def emailSES(destination, subject, body):
     if destination and subject and body:
         with mail.get_connection(backend=EMAIL_BACKEND) as connection:
             try:
-                response = mail.EmailMessage(subject=subject, body=body,
+                email = mail.EmailMessage(subject=subject, body=body,
                                         from_email=DEFAULT_FROM_EMAIL, to=destination,
-                                        connection=connection).send(fail_silently=False)
+                                        connection=connection)
+                email.content_subtype = "html"
+                response = email.send(fail_silently=False)
                 if response != 1:
                     raise Exception('Message not delivered. Contact Admin for more details.')
             except BadHeaderError:
