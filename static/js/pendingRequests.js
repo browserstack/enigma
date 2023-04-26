1d685fb232b4e5e3d8e05c4c09efde953a77fe27
Nivesh Mittapally <mittapally.nivesh@gmail.com>
2023-04-26 12:28:54 +0530

    feat: added js scripts for decline request

--- static/js/pendingRequests.js
+++ static/js/pendingRequests.js
@@ -1,26 +1,47 @@
 function closeNotification(){
-  $("#notification_bar").addClass("hidden")
+  $("#notification_bar").hide();
 }
 
-function showDeclineModal() {
-  $('#decline_modal').removeClass("hidden")
+function showDeclineModal(request_id) {
+  $('#decline_modal').show();
+  $(".declineHeading").html("<p class='text-lg leading-6 font-medium'>"+request_id+"<p/>")
+
 }
 
-const clickDeclineFinalButton =  () => {
-  $('#decline_modal').addClass("hidden")
+function closeDeclineModal() {
+  $('#decline_modal').hide();
+}
+
+const clickDeclineFinalButton = () => {
+  closeDeclineModal();
   showNotificiation("failed", "Request Declined");
+}
+
+function onChangeDeclineReason() {
+  if($("#declineReason").val() === "customInput") {
+    $("#otherType").show();
+    $("#reason_box").prop('required',true);
+  }
+  else {
+    $("#otherType").hide();
+    $("#reason_box").prop('required',false);
+    setDeclineReason($("#declineReason").val());
+  }
+}
+
+function setDeclineReason(reason) {
+  $("#declineReasonText").val(reason);
 }
 
 function showNotificiation(type, message) {
   if(type === "success"){
-    $(".notification_success").removeClass("hidden")
-    $(".notification_fail").addClass("hidden")
-    console.log("success")
+    $(".notification_success").show();
+    $(".notification_fail").hide();
   }
   else if(type === "failed") {
-    $(".notification_success").addClass("hidden")
-    $(".notification_fail").removeClass("hidden")
+    $(".notification_success").hide();
+    $(".notification_fail").show();
   }
-  $(".notification_message").html(message)
-  $("#notification_bar").removeClass("hidden")
+  $(".notification_message").html(message);
+  $("#notification_bar").show();
 }
