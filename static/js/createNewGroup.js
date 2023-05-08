83fcbb997873546e738b591c61601a544d12fa21
Rahul Kumar Yadav <rahul.y@browserstack@gmail.com>
2023-05-08 16:19:24 +0530

    fixes in creategroup ui

--- static/js/createNewGroup.js
+++ static/js/createNewGroup.js
@@ -1,48 +1,47 @@
 const handleSelectionView = () => {
-    const finalCount = $('#member-selection-table').children('tr').length;
+  const finalCount = $('#member-selection-table').children('tr').length;
 
-    if(finalCount < 1) {
-      $('#member-selection-empty').show();
-      $('#member-selection-list').hide();
-      $('#member-selection-list').children('nav').hide();
-      $('#member-selected-count').hide();
-    } else {
-      $('#member-selection-empty').hide();
-      $('#member-selection-list').show();
-      $('#member-selection-list').children('nav').css('display', 'flex');
-      $('#member-selected-count').show();
-      $('#member-selected-count').text(finalCount + ' selected');
-    }
-  };
+  if (finalCount < 1) {
+    $('#member-selection-empty').show();
+    $('#member-selection-list').hide();
+    $('#member-selection-list').children('nav').hide();
+    $('#member-selected-count').hide();
+  } else {
+    $('#member-selection-empty').hide();
+    $('#member-selection-list').show();
+    $('#member-selection-list').children('nav').css('display', 'flex');
+    $('#member-selected-count').show();
+    $('#member-selected-count').text(finalCount + ' selected');
+  }
+};
 
 const selectAllToggleSelection = (source) => {
   checkboxes = $('[name="selectedUserList"]');
 
-  for(var i=0, n=checkboxes.length; i<n; i++){
-      if(source.checked && checkboxes[i].checked==false){
-          checkboxes[i].checked = source.checked;
-          handleMemberSelection({elem: checkboxes[i], username: checkboxes[i].getAttribute("username"), firstName: checkboxes[i].getAttribute("firstName"), lastName: checkboxes[i].getAttribute("lastName"), email: checkboxes[i].getAttribute("value")})
-      }else if(source.checked == false){
-          checkboxes[i].checked = source.checked;
-          handleMemberSelection({elem: checkboxes[i], username: checkboxes[i].getAttribute("username"), firstName: checkboxes[i].getAttribute("firstName"), lastName: checkboxes[i].getAttribute("lastName"), email: checkboxes[i].getAttribute("value")})
-      }
+  for (var i = 0, n = checkboxes.length; i < n; i++) {
+    if (source.checked && checkboxes[i].checked == false) {
+      checkboxes[i].checked = source.checked;
+      handleMemberSelection({ elem: checkboxes[i], username: checkboxes[i].getAttribute("username"), firstName: checkboxes[i].getAttribute("firstName"), lastName: checkboxes[i].getAttribute("lastName"), email: checkboxes[i].getAttribute("value") })
+    } else if (source.checked == false) {
+      checkboxes[i].checked = source.checked;
+      handleMemberSelection({ elem: checkboxes[i], username: checkboxes[i].getAttribute("username"), firstName: checkboxes[i].getAttribute("firstName"), lastName: checkboxes[i].getAttribute("lastName"), email: checkboxes[i].getAttribute("value") })
+    }
   }
 };
 
 const selectMemberSelectionCheckbox = (username, checked) => {
-  if(checked) {
+  if (checked) {
     $(`#${username}-marker`).show();
     $(`#${username}-checkbox`).prop('checked', true);
-    $(`#${username}-tablerow`).removeClass('hover:bg-blue-50 hover:text-blue-700');
     $(`#${username}-tabledesc`).removeClass('text-gray-900').addClass('text-black-600');
-    $(`#${username}-tabledesc`).addClass('font-bold');
+    $(`#${username}-tabledesc`).addClass('font-semibold');
   } else {
     $('#selectAllUsers').prop('checked', false)
     $(`#${username}-marker`).hide();
     $(`#${username}-checkbox`).prop('checked', false);
-    $(`#${username}-tablerow`).removeClass('bg-gray-50').addClass('hover:bg-blue-50 hover:text-blue-700');
+    $(`#${username}-tablerow`).removeClass('bg-gray-50').addClass('hover:bg-blue-50');
     $(`#${username}-tabledesc`).removeClass('text-black-600');
-    $(`#${username}-tabledesc`).removeClass('font-bold');
+    $(`#${username}-tabledesc`).removeClass('font-semibold');
   }
 };
 
@@ -54,9 +53,10 @@ const addMemberSelection = (username, firstName, lastName, email) => {
   newSpan.appendTo(selectionList);
   newSpan.show();
   const tableData = newSpan.children('td');
-  tableData[0].textContent = firstName+" "+lastName+" "+email;
+  tableData[0].textContent = firstName + " " + lastName + " ";
   newSpan.attr('id', `member-selection-${username}`);
   newSpan.attr('email', email)
+  newSpan.addClass('font-semibold text-sm');
   id = `member-selection-${username}`
 
   handleSelectionView();
@@ -83,63 +83,63 @@ const removeMemberSelectionUI = (elem) => {
 
 const removeAllMembers = () => {
   const members = $('#member-selection-table').children('tr');
-  for(iter = 0; iter < members.length; iter++) {
+  for (iter = 0; iter < members.length; iter++) {
     removeSelectionSpanElem(members[iter]);
   }
 };
 
-const handleMemberSelection = ({elem, username, firstName, lastName, email}) => {
-  if(elem.checked) {
+const handleMemberSelection = ({ elem, username, firstName, lastName, email }) => {
+  if (elem.checked) {
     addMemberSelection(username, firstName, lastName, email);
   } else {
     removeMemberSelection(username);
   }
 };
 
-$(function(){
-    $('#newGroupName').on('input', function(){
-        var groupName = $(this).val();
-        var groupNamePattern = /[A-Za-z0-9][A-Za-z0-9-]{2,}/;
-        var error = document.getElementById("invalidGroupName")
-        if(groupNamePattern.test(groupName)){
-            $(this).removeClass("border-red-500");
-            error.classList.add("hidden");
-        }else{
-            $(this).addClass("border-red-500");
-            error.classList.remove("hidden");
-        }
-    });
+$(function () {
+  $('#newGroupName').on('input', function () {
+    var groupName = $(this).val();
+    var groupNamePattern = /[A-Za-z0-9][A-Za-z0-9-]{2,}/;
+    var error = document.getElementById("invalidGroupName")
+    if (groupNamePattern.test(groupName)) {
+      $(this).removeClass("border-red-500");
+      error.classList.add("hidden");
+    } else {
+      $(this).addClass("border-red-500");
+      error.classList.remove("hidden");
+    }
+  });
 });
 
-$(function(){
-    $('#newGroupReason').on('input', function(){
-        var groupReason = $(this).val();
-        var groupReasonPattern = /.+/;
-        var error = document.getElementById("invalidGroupReason")
-        if(groupReasonPattern.test(groupReason)){
-            $(this).removeClass("border-red-500");
-            error.classList.add("hidden");
-        }else{
-            $(this).addClass("border-red-500");
-            error.classList.remove("hidden");
-        }
-    });
+$(function () {
+  $('#newGroupReason').on('input', function () {
+    var groupReason = $(this).val();
+    var groupReasonPattern = /.+/;
+    var error = document.getElementById("invalidGroupReason")
+    if (groupReasonPattern.test(groupReason)) {
+      $(this).removeClass("border-red-500");
+      error.classList.add("hidden");
+    } else {
+      $(this).addClass("border-red-500");
+      error.classList.remove("hidden");
+    }
+  });
 });
 
 function showNotificiation(type, message, title) {
-    if(type === "success"){
-      $(".notification_success").removeClass("hidden")
-      $(".notification_fail").addClass("hidden")
-      console.log("success")
-    }
-    else if(type === "failed") {
-      $(".notification_success").addClass("hidden")
-      $(".notification_fail").removeClass("hidden")
-    }
-    $(".notification_message").html(message)
-    $(".notification_title").html(title)
-    $("#notification_bar").removeClass("hidden")
+  if (type === "success") {
+    $(".notification_success").removeClass("hidden")
+    $(".notification_fail").addClass("hidden")
+    console.log("success")
   }
+  else if (type === "failed") {
+    $(".notification_success").addClass("hidden")
+    $(".notification_fail").removeClass("hidden")
+  }
+  $(".notification_message").html(message)
+  $(".notification_title").html(title)
+  $("#notification_bar").removeClass("hidden")
+}
 
 function showRequestSuccessMessage(message) {
   const successMessage = document.getElementById('request-success-message')
@@ -150,11 +150,11 @@ function showRequestSuccessMessage(message) {
   modalOverlay.classList.remove("hidden");
 }
 
-function closeNotification(){
+function closeNotification() {
   $("#notification_bar").addClass("hidden")
 }
 
-$(document).on('click', '#submitNewGroup', function(){
+$(document).on('click', '#submitNewGroup', function () {
   const members = $('#member-selection-table').children('tr');
   const emails = [];
   var isValid = true;
@@ -163,8 +163,8 @@ $(document).on('click', '#submitNewGroup', function(){
   var newGroupReasonPattern = /.+/;
   var urlBuilder = "/group/create"
 
-    
-  for(iter = 0; iter < members.length; iter++) {
+
+  for (iter = 0; iter < members.length; iter++) {
     emails.push($(members[iter])[0].getAttribute("email"));
   }
 
@@ -173,7 +173,7 @@ $(document).on('click', '#submitNewGroup', function(){
   const newGroupReason = document.getElementById('newGroupReason').value;
   const requiresAccessApprove = document.getElementById('requiresAccessApprove').checked;
 
-  if(!newGroupNamePattern.test(newGroupName)){
+  if (!newGroupNamePattern.test(newGroupName)) {
     var error = document.getElementById("invalidGroupName");
     var newGroup = document.getElementById("newGroupName");
     newGroup.classList.add("border-red-500");
@@ -181,7 +181,7 @@ $(document).on('click', '#submitNewGroup', function(){
     isValid = false;
   }
 
-  if(!newGroupReasonPattern.test(newGroupReason)){
+  if (!newGroupReasonPattern.test(newGroupReason)) {
     var error = document.getElementById("invalidGroupReason");
     var newGroup = document.getElementById("newGroupReason");
     newGroup.classList.add("border-red-500");
@@ -189,10 +189,11 @@ $(document).on('click', '#submitNewGroup', function(){
     isValid = false;
   }
 
-  if(!isValid)
+  if (!isValid)
     return;
-  else{
-    $.ajax({url: urlBuilder,
+  else {
+    $.ajax({
+      url: urlBuilder,
       type: "POST",
       data: {
         newGroupName: newGroupName,
@@ -201,14 +202,15 @@ $(document).on('click', '#submitNewGroup', function(){
         selectedUserList: selectedUserList,
         csrfmiddlewaretoken: csrf_token
       },
-      success: function(result){
+      success: function (result) {
         var msg = result.status.msg;
         showRequestSuccessMessage(msg)
       },
-      error: function(response){
+      error: function (response) {
         var error_title = response.responseJSON.error.error_msg;
         var error_message = response.responseJSON.error.msg;
         showNotificiation("failed", error_message, error_title)
-      }});
+      }
+    });
   }
 });
