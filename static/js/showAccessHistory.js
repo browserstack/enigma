413be58748ee1d45ffc77ecc50687217095f6326
Rahul Kumar Yadav <rahul.y@browserstack@gmail.com>
2023-05-23 23:38:22 +0530

    feat:show a div on hovering accessess

--- static/js/showAccessHistory.js
+++ static/js/showAccessHistory.js
@@ -2,7 +2,7 @@ let currentStatusFilterState = 'hidden';
 
 const focusFilterStatus = (elem) => {
   const ulElem = $(elem).children('ul');
-  if(currentStatusFilterState == 'hidden') {
+  if (currentStatusFilterState == 'hidden') {
     currentStatusFilterState = 'shown';
     ulElem.show();
   } else {
@@ -16,7 +16,7 @@ const selectLI = (elem) => {
   const checkboxSpan = $(elem).children('span')[0];
   const textSpan = $(elem).children('span')[1];
 
-  if(inputElem.is(':checked')) {
+  if (inputElem.is(':checked')) {
     $(elem).removeClass("text-white bg-blue-600").addClass("text-gray-900");
     inputElem.prop('checked', false);
     $(checkboxSpan).removeClass("text-white").addClass("text-blue-600");
@@ -28,3 +28,32 @@ const selectLI = (elem) => {
     $(textSpan).removeClass("font-normal").addClass("font-semibold");
   }
 };
+
+
+function showInfoDiv(element, request_id, requested_on, updated_on) {
+  console.log('displaying....')
+  console.log('updated_on-->', requested_on)
+  var infoDiv = $(`#infoDiv-${request_id}`);
+  infoDiv.removeClass("hidden");
+
+  var currentDate = new Date();
+
+  var requested_date = new Date((requested_on).split(',').slice(0, 2).join(','));
+
+  var diffMilliseconds = currentDate - requested_date;
+  var diffSeconds = diffMilliseconds / 1000;
+  var diffMinutes = diffSeconds / 60;
+  var diffHours = diffMinutes / 60;
+  var diffDays = diffHours / 24;
+  diffDays = Math.round(diffDays);
+  infoDiv.children('div').children('div#process').children('span.text-slate-400').text(`${diffDays} days`)
+
+}
+
+function hideInfoDiv(elem, request_id) {
+  var infoDiv = $(`#infoDiv-${request_id}`);
+  infoDiv.addClass("hidden");
+  console.log("hiding...")
+  // infoDiv.classList.add("hidden");
+}
+
