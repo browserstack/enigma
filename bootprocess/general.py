bd6ca9dbe26f28f0f463c3eb3a321135ce405229
Sharad Sharma <sharad@browserstack.com>
2023-05-24 10:52:35 +0530

    fix: lint issues for bootprocess

--- bootprocess/general.py
+++ bootprocess/general.py
@@ -1,22 +1,25 @@
+""" file contains general methods for bootprocess """
+import logging
 from django.core import mail
 from django.core.mail import BadHeaderError
-import logging
 from EnigmaAutomation.settings import EMAIL_BACKEND, DEFAULT_FROM_EMAIL
 logger = logging.getLogger(__name__)
 
-def emailSES(destination, subject, body):
+
+def emailSES(destination, subject, body):   # noqa: C0103
+    """ method to send email to destination with given subject and body """
     if destination and subject and body:
         with mail.get_connection(backend=EMAIL_BACKEND) as connection:
             try:
                 email = mail.EmailMessage(subject=subject, body=body,
-                                        from_email=DEFAULT_FROM_EMAIL, to=destination,
-                                        connection=connection)
+                                          from_email=DEFAULT_FROM_EMAIL, to=destination,
+                                          connection=connection)
                 email.content_subtype = "html"
                 response = email.send(fail_silently=False)
                 if response != 1:
                     raise Exception('Message not delivered. Contact Admin for more details.')
-            except BadHeaderError:
-                raise Exception("Invalid header found.")
+            except BadHeaderError as exc:
+                raise Exception("Invalid header found.") from exc
     else:
         raise Exception('Make sure all fields are entered and valid.')
 
