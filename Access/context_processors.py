2b34a3551337b212dd7286dd3ea8decaff1688c7
Nivesh Mittapally <mittapally.nivesh@gmail.com>
2023-06-06 08:15:28 +0530

    Feat: Pending actions UI improvements (#78)
    * feat: added ui changes for pendingRequests
    
    * feat: added modal and notifications to pending request UI
    
    * refactor: removed user profile from current changes
    
    * refactor: added separate js file for pending request
    
    * refactor: resolve conflicts
    
    * refactor: added separate js file
    
    * feat: added pending actions count function
    
    * feat: added js scripts for decline request

--- Access/context_processors.py
+++ Access/context_processors.py
@@ -19,5 +19,6 @@ def add_variables_to_context(request):
 
     context["totalAccessCount"] = currentUser.get_total_access_count()
     context["groupsMemberFor"] = len(currentUser.get_active_groups())
+    context["pendingActionsCount"] = currentUser.getPendingApprovalsCount(all_access_modules)
 
     return context
